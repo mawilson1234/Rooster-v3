adc3d8847b2c2ead645c6a1d3fde8e935418f69e
mawilson1234 <mawilson@linguist.umass.edu>
2020-12-05 15:23:43 -0500

    Updated instructions

--- data_includes/Rooster.js
+++ data_includes/Rooster.js
@@ -84,7 +84,7 @@ var items = [
 
     ['practice', Message, {consentRequired: false,
                        html: ["div",
-                               ["p", "How was that? It is important that you move through the sentence carefully and deliberately, making sure that you understand each word and create a mental picture as you go."],
+                               ["p", "How was that? It is important that you move through the sentence carefully and deliberately, making sure that you understand as you go."],
                                ["p", "Let's try some more."]
                              ]}],
 
@@ -1371,7 +1371,7 @@ var items = [
                                                         Separator, {transfer: 1000, normalMessage: "+", ignoreFailure: true}],
     [["Rooster-ExpV-Stative-ActS-ActQ-AdverbQ-AnsR", 20], Separator, {transfer: 1000, normalMessage: "+"},
                                                         sentence, {s: "That's the editor that the journalist somewhat depresses despite his upbeat personality."},
-                                                        question, {q: "How much does the journalist depress the editor?", as: [["f", "hugely"], ["j", "to an extent"]], hasCorrect: 1}
+                                                        question, {q: "How much does the journalist depress the editor?", as: [["f", "hugely"], ["j", "to an extent"]], hasCorrect: 1},
                                                         Separator, {transfer: 1000, normalMessage: "+", ignoreFailure: true}],
     [["Rooster-ExpV-Eventive-ActS-ActQ-AdverbQ-AnsR", 20], Separator, {transfer: 1000, normalMessage: "+"},
                                                         sentence, {s: "That's the editor that the journalist accidentally depresses despite his upbeat personality."},
